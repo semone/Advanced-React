75e9b7e0098f7ccf358e23ad89202ffab8a4f524
Semone Kallin Thander <semone.kallinthander@collectorbank.se>
2018-10-30 21:53:22 +0100

    add first query and mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,14 @@
-const mutations = {};
+const mutations = {
+	// TODO: check if logged in
+
+	async createItem(parent, args, context, info) {
+		const item = await context.db.mutation.createItem({
+			data: {
+				...args
+			}
+		}, info);
+		return item;
+	}
+};
 
 module.exports = mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,11 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+	items: forwardTo('db'),
+	// async items(parent, arg, context, info) {
+	// 	const item = await context.db.query.items();
+	// 	return item;
+	// }
+};
 
 module.exports = Query;
