5c9d3a133503c26e91fa498cb8fccc822223a1d3
Semone Kallin Thander <semone.kallinthander@collectorbank.se>
2018-10-31 22:19:10 +0100

    react meets graphql

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import App, { Container } from 'next/app';
+import { ApolloProvider } from 'react-apollo';
 import Page from '../components/Page';
+import withData from '../lib/withData';
 
 class MyApp extends App {
-  render() {
-    const { Component } = this.props;
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
 
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, apollo, pageProps } = this.props;
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     );
   }
 }
 
-export default MyApp;
+export default withData(MyApp);
--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,6 +1,8 @@
+import Items from '../components/Items';
+
 const Home = () => (
   <div>
-    <p> Home </p>
+    <Items />
   </div>
 );
 export default Home;
