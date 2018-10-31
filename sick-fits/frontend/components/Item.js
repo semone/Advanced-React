104b9ee3fea4504bfb585b95cb49eef4c25fc551
Semone Kallin Thander <semone.kallinthander@collectorbank.se>
2018-10-31 22:25:06 +0100

    react meets graphql 2

--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -22,7 +22,7 @@ class Item extends Component {
     const { item } = this.props;
     return (
       <ItemStyles>
-        {item.image && <img src="{item.image}" alt="{item.title}" />}
+        {item.image && <img src={item.image} alt={item.title} />}
         <Title>
           <Link
             href={{
